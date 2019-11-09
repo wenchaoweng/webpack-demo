f412dd0f46ee45d0014f9cb349f2ce56a7cee6be
wenchao <397385020@qq.com>
2019-11-09 13:55:49 +0800

    添加工程文件

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import '../css/style.scss'
+let abcd = 2
+console.log(abcd)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode:'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          // add vendor prefixes to CSS
+          'postcss-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
+    ]
+  }
+};
